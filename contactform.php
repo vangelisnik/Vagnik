64858cd4cbe292ff35d52f2b3c0fc4e0d073e33b
vangelisnik <77838391+vangelisnik@users.noreply.github.com>
2021-01-22 14:50:08 +0200

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,16 +1,11 @@
-<?php
-
-   
+<?php 
    $name= $_POST['name'];
-   $Subject = $_POST['Subject'];
-   $mailFrom = $_POST['Email'];
-   $Message = $_POST['Message'];
-
-   $mailTo = "nikolopoulosvangelis@gmail.com";
-   $headers = "From: ".$mailFrom;
-   $txt = "You have received an e-mail from ".$Name.".\n\n".$Message;
-
-   mail($mailTo,$Subject,$text,$headers);
-   header("Location: index.html?mailsend")
-}
+   $email= $_POST['email']; 
+   $message= $_POST['message'];
+   $formcontent="From: $name /n Message: $message";
+   $recipient = "nikolopoulosvangelis@gmail.com"
+   $subject = "Contact Form";
+   $mailheader = "From: $email /r/n";
+   mail($recipient,$subject,$formcontent,$mailheader) or die("Error!")
+   echo "Thank You"; 
 ?>
