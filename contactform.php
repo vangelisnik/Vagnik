c959de583ef79d925a41c0ecdf570d3ff4cdb416
vangelisnik <77838391+vangelisnik@users.noreply.github.com>
2021-01-22 15:25:01 +0200

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -18,14 +18,14 @@ $mail_status = mail($mail_to, $subject, $body_message, $headers);
 if ($mail_status) { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Sucess Message');
-		window.location = 'contact.html';
+		window.location = 'index.html';
 	</script>
 <?php
 }
 else { ?>
 	<script language="javascript" type="text/javascript">
 		alert('Error Message');
-		window.location = 'contact.html';
+		window.location = 'index.html';
 	</script>
 <?php
 }
