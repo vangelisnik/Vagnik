295f26ecdebc6e41d1c352cc91e34c5c826696da
vangelisnik <77838391+vangelisnik@users.noreply.github.com>
2021-01-22 15:19:52 +0200

    Update contactform.php

--- contactform.php
+++ contactform.php
@@ -1,11 +1,32 @@
-<?php 
-   $name= $_POST['name'];
-   $email= $_POST['email']; 
-   $message= $_POST['message'];
-   $formcontent="From: $name /n Message: $message";
-   $recipient = "nikolopoulosvangelis@gmail.com"
-   $subject = "Contact Form";
-   $mailheader = "From: $email /r/n";
-   mail($recipient,$subject,$formcontent,$mailheader) or die("Error!")
-   echo "Thank You"; 
+<?php
+$field_name = $_POST['cf_name'];
+$field_email = $_POST['cf_email'];
+$field_message = $_POST['cf_message'];
+
+$mail_to = 'nikolopoulosvangelis@gmail.com';
+$subject = 'Your Subject'.$field_name;
+
+$body_message = 'From: '.$field_name."\n";
+$body_message .= 'E-mail: '.$field_email."\n";
+$body_message .= 'Message: '.$field_message;
+
+$headers = 'From: '.$field_email."\r\n";
+$headers .= 'Reply-To: '.$field_email."\r\n";
+
+$mail_status = mail($mail_to, $subject, $body_message, $headers);
+
+if ($mail_status) { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Sucess Message');
+		window.location = 'contact.html';
+	</script>
+<?php
+}
+else { ?>
+	<script language="javascript" type="text/javascript">
+		alert('Error Message');
+		window.location = 'contact.html';
+	</script>
+<?php
+}
 ?>
